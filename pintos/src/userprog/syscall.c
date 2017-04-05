9edafe1919e4b4d283c3893ca39b737987e22f46
Yekaterina Abileva <k.abileva@gmail.com>
2017-04-06 02:55:17 +0900

    34 of 76 tests failed: some system calls added

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,15 +7,18 @@
 #include "userprog/process.h"
 #include <lib/kernel/console.h>
 #include "threads/synch.h"
+#include "filesys/filesys.h"
 
-static int get_int_32(const void*);
-static int get_user(const uint8_t*);
+static int ptr_to_int(const void*);
 static void sys_exit(int);
 static void sys_write(struct intr_frame *f);
 
 static void syscall_handler (struct intr_frame *);
+static void sys_create(struct intr_frame *f);
+static void sys_open(struct intr_frame *f);
+static void sys_read(struct intr_frame *f);
 
-static struct lock file_sys_lock;
+static struct lock files_lock;
 
 
 void exit(int code)
@@ -28,31 +31,39 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-  lock_init(&file_sys_lock);
+  lock_init(&files_lock);
 
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-	int reg = get_int_32(f->esp); 
+	int reg = ptr_to_int(f->esp); 
 
 switch(reg)
 	{
- 	case SYS_EXIT: sys_exit(get_int_32(f->esp+4));
-			break;
+ 	case SYS_EXIT: sys_exit(ptr_to_int(f->esp+4));
+		break;
 	case SYS_WRITE: 
-			sys_write(f); break;
-
-	
+		sys_write(f); break;
+	case SYS_CREATE:
+		sys_create(f);
+		break;
+	case SYS_OPEN:
+		sys_open(f);
+		break;
+	case SYS_CLOSE:
+		break;
+	case SYS_READ:
+		sys_read(f);
+		break;
 	default:
-	{
+		{
  		exit(-1);
+		}
 	}
 }
-		
-  //thread_exit ();
-}
+
 
 static void sys_exit(int code) {
 
@@ -60,16 +71,54 @@ static void sys_exit(int code) {
 	thread_exit ();
 	NOT_REACHED ();
 }
+static void sys_open(struct intr_frame *f) {
+	
+	/* Pull arguments from stack */
+	char *name = (char*)ptr_to_int(f->esp+4);
+	if(name==NULL) return;
+
+	lock_acquire(&files_lock);
+	/* Try to open the file */
+	if(filesys_open(name)==NULL) f->eax=-1; 
+	lock_release(&files_lock);
+
+}
+
+static void sys_read(struct intr_frame *f) {
+	const char* buf;
+ 	size_t size;
+
+ 	size= ptr_to_int(f->esp+12);
+ 	buf = ptr_to_int(f->esp+8);
+ 	/* Validate the pointer */
+ 	if (buf+size-1 >= PHYS_BASE) exit(-1);
+
+}
+
+
+static void sys_create(struct intr_frame *f) {
+	bool created;
+	/* Pull arguments from stack */
+
+	char *file = (char*)ptr_to_int(f->esp+4);
+	int initial_size = (size_t)ptr_to_int(f->esp+8);
+	lock_acquire(&files_lock);
+	if(file==NULL) exit(-1);
+	created = filesys_create (file,initial_size); 
+	lock_release(&files_lock);
+
+	f->eax = created;
+}
 
  static void sys_write(struct intr_frame *f)
  {
  	const char* buf;
  	size_t size;
 
- 	size= get_int_32(f->esp+12);
+ 	size= ptr_to_int(f->esp+12);
 	
- 	int fd = get_int_32(f->esp+4);
- 	
+ 	int fd = ptr_to_int(f->esp+4);
+
  	if (fd==0) 
  		f->eax=-1;
  	else if (fd==1) {
@@ -85,25 +134,13 @@ static void sys_exit(int code) {
 	return -1;
 }	
 
-static int get_user(const uint8_t* uaddr)
-{
-	int result;
-	asm("movl $1f, %0; movzbl %1, %0; 1:"
-			: "=&a" (result) : "m" (*uaddr));
-	return result;
-}
+/* Function for converting esp register to the int value */
 
-static int get_int_32(const void* ptr_)
+static int ptr_to_int(const void* ptr)
  
    {
-   	if (ptr_ >= PHYS_BASE) exit(-1);
-	uint8_t *ptr = ptr_;
-	int i;
-	for (i = 0; i < 4; ++i)
-	{
-		if (get_user(ptr+i) == -1)
-			exit(-1);
-	}
+    if (ptr>= PHYS_BASE) exit(-1);
+	
 	return *((int *)ptr);
 }
 
